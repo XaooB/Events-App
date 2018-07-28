84ef9b9262e8a265494810312a28a2f8525f2436
XaooB <prawo123@interia.pl>
2018-07-28 13:49:05 +0200

    basic configuration for indexeddb

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,34 +1,55 @@
 const Events = {
   //initial values that will be added to the database after visiting  website and resetting current database state
   initialValues: [1,2,3],
+  indexedDB: window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB,
+  dbOpen: this.indexedDB.open('Events', 2),
 
   bindEvents: function() {
     let modal = document.querySelector('.modal'),
         addEventBtn = modal.querySelector('#add-wrapper');
 
-    //testing purposes
     addEventBtn.addEventListener('click', (e) => {
-      console.log('btn clicked');
+      this.addEvent();
     });
-  },
-  createDatabase: function()  {
-    //create initial indexedDb
   },
   createDomElement: function() {
     //create document element
   },
-  addEvent: function(eventID) {
-    //add event to the database
+  addInitialValues: function() {
+    // let db = dbOpen.result,
+    //     tx = db.transaction('name', 'readwrite'),
+    //     store = tx.objectStore('name'),
+    //     eventList = tx.objectStore.getAll();
+
+    //this.initialValues.forEach((item, key) => {
+    //  store.put({title: item.title, location: item.location, describe: item.describe, date: item.date, image: item.image})
+    //})
+    //
+    // for(let item in eventList) {
+    //   //createDomElement and add to the DOM
+    // }
+  },
+  addEvent: function() {
+    //data from form
+    //store.put({title: '', location: '', describe: '', date: '', image: ''})
   },
   deleteEvent: function(eventID) {
     //detele event from the database
   },
   displayEvents: function(list) {
-    //display events through DOM
+
   },
   //initial funtion
   initial: function() {
-    this.bindEvents();
+    this.dbOpen.onupgradeneeded = e => {
+      console.log('onupgradeneeded event ', e);
+    }
+    this.dbOpen.onsuccess = e => {
+      console.log('onsuccess event ',e);
+    }
+    this.dbOpen.onerror = e => {
+      console.log('onerror event ', e);
+    }
   }
 }
 
