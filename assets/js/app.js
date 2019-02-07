42b09c282ff104c26c0c32c62ff461af3ba8b5a2
XaooB <prawo123@interia.pl>
2019-02-07 15:35:16 +0100

    unnoticed changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,7 @@ const Events = {
   initialValues: [
     {
       title: '5th CEPPIS Conference',
-      location: 'Saturday at 6 pm, H15 Boutique Hotel, Warszawa',
+      location: 'Warszawa',
       description: 'International scientific conference Civil Engineering: Present Problems.',
       summary: 'We encourage you to take part in a discussion in the course of the conference debates or present your theoretical or practical considerations, experiences and results of conducted investigations. Innovative Solutions CEPPIS 2019 is set out to pay attention to recent trends especially Circular Economy in Construction Sector.',
       date: `2019-5-22`,
@@ -12,7 +12,7 @@ const Events = {
     },
     {
       title: 'Up In Smoke Tour',
-      location: 'Saturday at 5 pm, Mattress Firm Amphitheatre, 2050 Entertainment Cir, Nowy Jork',
+      location: 'Nowy Jork',
       description: 'Featured Eminem, Snoop Dog, Dr Dre and more! Best event of 2018',
       summary: "The tour was originally called The Boyz in the Hood under the pretext of Dr. Dre's to-be-released collaborative album The Chronic 2000 (later renamed).[1] In September 1999, Snoop Dogg stated that he, Dr. Dre, Eminem, Xzibit, Warren G and Nate Dogg would form the line-up.[2] By April 2000, Ice Cube was on board as part of the tour, which was slated for a June 15 start in San Diego. MC Ren, one time MC of N.W.A, was expected to join the tour in order to have reunited version of N.W.A along with Dr. Dre, Ice Cube, and Snoop Dogg.[3] Come May the tour was officially known as the Up In Smoke Tour,[4] and Dr. Dre was promising fans. Its gonna be incredible. We are gonna give everybody that's been buying our records a real show, something they've never seen before.",
       date: `2018-9-02`,
@@ -21,7 +21,7 @@ const Events = {
     },
     {
       title: 'Laravel #Poznań #Meetup #9!',
-      location: 'Sunday at 11 am, Lincoln Street, Londym',
+      location: 'Sunday at 11 am, Lincoln Street, Londyn',
       description: "Laravel's freaks and not only let's connect!",
       summary: 'Already in the New Year we invite you to the next editions of Laravel Meetups! Follow us to find out what we have prepared for you this time! It willl be possible to ask questions by Sli. Do application. We want to reach as many fans of Laravel as possible. We are going to share live streaming on our profile, especially for those who cannot be with us. We will try to release recordings of presentations after meetup (share on. slideshare.net/Laravel_Poznan_Meetup)',
       date: `2018-9-10`,
@@ -30,7 +30,7 @@ const Events = {
     },
     {
       title: 'Open Day - Business Link Maraton',
-      location: 'Maraton Business Center, Poznań',
+      location: 'Poznań',
       description: 'We have a good reason to make you visit us at MBC. We will meet and talk about business and economy',
       summary: 'Young Living - to od 25 lat światowy lider w dziedzinie aromaterapii, ekologiczne farmy, przyjazne biura i	6-milionowa społeczność w 40 krajach, na wszystkich kontynentach. Jednak przede wszystkim to filozofia czystych i aktywnych biologicznie olejów eterycznych, których produkcja jest kontrolowana na każdym etapie - od ziarna do szklanej buteleczki. Tylko 2% olejków eterycznych na świecie ma takie naturalne, biologicznie czynne pochodzenie zgodne ze standardem "from Sead to Seal"',
       date: `2018-9-30`,
